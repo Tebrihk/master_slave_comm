c87f67483280fc2c3e8537528ace1d5ea4acb6af
Tebrihk Ibrahim <123522034+Tebrihk@users.noreply.github.com>
2025-01-15 14:12:33 +0100

    Update master.ino to accomodate another slave node

--- master/master.ino
+++ master/master.ino
@@ -2,104 +2,140 @@
 #include <RF24.h>
 #include <RF24Network.h>
 
-RF24 radio(7, 8);  
-RF24Network network(radio);  
+RF24 radio(7, 8);
+RF24Network network(radio);
+
+const uint16_t office_node = 01;
+const uint16_t master_node = 00;
+const uint16_t reception_node = 02;
+
+const unsigned long debounce_delay = 200;
+
+unsigned long last_action_time_red = 0, last_action_time_green = 0, last_action_time_blue = 0;
+unsigned long last_button_pressed = 0;
+unsigned long packets_sent = 0;
+
 int bulbGreen = 4;
 int bulbBlue = 5;
-int bulbRed = 3;
-int button = 6; 
-
-const uint16_t this_node = 01;   
-const uint16_t other_node = 00;  
-
-const unsigned long interval = 2000;  
-
-unsigned long last_sent;     
-unsigned long packets_sent; 
-unsigned long last_action_time = 0;
-unsigned long last_button_pressed = 0;
-const unsigned long debounce_delay = 200;
+int bulbRed = 7;
+int button1 = 6;
+int button2 = 2;
 
 bool greenBlink = false;
 bool redBlink = false;
 bool blueBlink = false;
-bool ONgreen = false; 
-bool ONred = false; 
+bool ONgreen = false;
+bool ONred = false;
+bool ONblue = false;
 
-struct payload_t {  
+struct payload_t {
   unsigned long ms;
-  String counter;
+  unsigned long counter;
 };
 
-void setup(void) {
+void setup() {
   Serial.begin(9600);
-  pinMode(bulbGreen, OUTPUT);
-  pinMode(bulbBlue, OUTPUT);
-  pinMode(bulbRed, OUTPUT);
-  pinMode(button, INPUT_PULLUP);
-  while (!Serial) {
-  }
+  while (!Serial) {}
+
   Serial.println(F("MASTER"));
 
   if (!radio.begin()) {
     Serial.println(F("Radio hardware not responding!"));
-    while (1) {
-    }
+    while (1) {}
   }
+  
   radio.setChannel(90);
-  network.begin(this_node);
+  network.begin(master_node);
+
+  pinMode(bulbGreen, OUTPUT);
+  pinMode(bulbBlue, OUTPUT);
+  pinMode(bulbRed, OUTPUT);
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
 }
 
 void loop() {
-  network.update();  
-  if (digitalRead(button) == LOW && (millis() - last_button_pressed > debounce_delay)) {
-    last_button_pressed = millis();
-    digitalWrite(bulbBlue, LOW);
+  network.update();
 
-    payload_t payload = { 100, "green" };
-    RF24NetworkHeader header(other_node);
-    bool ok = network.write(header, &payload, sizeof(payload));
-    if (ok) {
-      Serial.println(F("CALL sent"));
-      greenBlink = true;
-      digitalWrite(bulbRed, LOW);
-      redBlink = false;   
-    } 
-    else {
-      Serial.println(F("CALL failed to send"));
-      redBlink = true;   
-    }  
-    last_action_time = millis();  
-  }
-
-  if (greenBlink && (millis() - last_action_time) > 500 && !ONgreen) {
-    digitalWrite(bulbGreen, HIGH);
-    ONgreen = !ONgreen;
-  } 
-  if (greenBlink && (millis() - last_action_time) > 1000 && ONgreen) {
-    digitalWrite(bulbGreen, LOW);
-    ONgreen = !ONgreen;
-    last_action_time = millis();
-  }
-
-  if (redBlink && (millis() - last_action_time) > 500 && !ONred) {
-    digitalWrite(bulbRed, HIGH);
-    ONred = !ONred;
-  } 
-  if (redBlink && (millis() - last_action_time) > 1000 && ONred) {
-    digitalWrite(bulbRed, LOW);
-    ONred = !ONred;
-    last_action_time = millis();
-  }
-
-  while (network.available()) {
-    RF24NetworkHeader header;
-    payload_t response;
-    network.read(header, &response, sizeof(response));
-    greenBlink = true;
-    if (greenBlink) {
-      greenBlink = false;
+  // Green LED Blink Logic
+  if (greenBlink) {
+    if ((millis() - last_action_time_green) > 500 && !ONgreen) {
+      digitalWrite(bulbGreen, HIGH);
+      ONgreen = true;
+    }
+    if ((millis() - last_action_time_green) > 1000 && ONgreen) {
       digitalWrite(bulbGreen, LOW);
+      ONgreen = false;
+      last_action_time_green = millis();
+    }
+  }
+
+  // Red LED Blink Logic
+  if (redBlink) {
+    if ((millis() - last_action_time_red) > 500 && !ONred) {
+      digitalWrite(bulbRed, HIGH);
+      ONred = true;
+    }
+    if ((millis() - last_action_time_red) > 1000 && ONred) {
+      digitalWrite(bulbRed, LOW);
+      ONred = false;
+      last_action_time_red = millis();
+    }
+  }
+
+  // Blue LED Blink Logic
+  if (blueBlink) {
+    if ((millis() - last_action_time_blue) > 500 && !ONblue) {
+      digitalWrite(bulbBlue, HIGH);
+      ONblue = true;
+    }
+    if ((millis() - last_action_time_blue) > 1000 && ONblue) {
+      digitalWrite(bulbBlue, LOW);
+      ONblue = false;
+      last_action_time_blue = millis();
+    }
+  }
+
+  // calling office
+  if (digitalRead(button1) == LOW && (millis() - last_button_pressed > debounce_delay )) {
+    last_button_pressed = millis();
+    payload_t payload = { millis(), packets_sent++ };
+    RF24NetworkHeader header(office_node);
+    bool success = network.write(header, &payload, sizeof(payload));
+    if (success) {
+      Serial.println(F("CALLING"));
+      greenBlink = true;
+    } else {
+      redBlink = true;
+    }
+  }
+
+  // Receive
+ while (network.available()) {
+    RF24NetworkHeader header;
+    payload_t payload;
+    if (network.read(header, &payload, sizeof(payload))) {
+      if (header.from_node == office_node) {
+        Serial.println("Office acknowledged the call");
+        greenBlink = false;
+      } else if (header.from_node == reception_node) {
+        Serial.println("Reception acknowledged the call");
+        blueBlink = false;
+      }
+    }
+  }
+
+// calling receptionist
+  if (digitalRead(button2) == LOW && (millis() - last_button_pressed > debounce_delay)) {
+    last_button_pressed = millis();
+    payload_t payload = { millis(), packets_sent++ };
+    RF24NetworkHeader header(reception_node);
+    bool success = network.write(header, &payload, sizeof(payload));
+    if (success) {
+      Serial.println(F("CALLING receptionist"));
+      blueBlink = true;
+    } else {
+      redBlink = true;
     }
   }
 }
